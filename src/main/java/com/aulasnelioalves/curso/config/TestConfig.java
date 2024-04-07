e95e7ff036e3aaf6b3d8be04d2f6107d7f5f4af2
VictorOliveiraJV <victoroliveirajv10@gmail.com>
2024-04-07 20:23:36 -0300

    Many-to-many Associoation with JoinTable

--- src/main/java/com/aulasnelioalves/curso/config/TestConfig.java
+++ src/main/java/com/aulasnelioalves/curso/config/TestConfig.java
@@ -50,7 +50,18 @@ public class TestConfig implements CommandLineRunner {
 		
 		
 		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		p1.getCategories().add(c2);
+		p2.getCategories().add(c3);
+		p2.getCategories().add(c1);
+		p3.getCategories().add(c3);
+		p4.getCategories().add(c3);
+		p5.getCategories().add(c2);
+		
+		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+
+		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "98788898", "1234568");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "84635655", "1234568");
--- src/main/java/com/aulasnelioalves/curso/entities/Category.java
+++ src/main/java/com/aulasnelioalves/curso/entities/Category.java
@@ -4,12 +4,14 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -21,7 +23,8 @@ public class Category {
 	private Integer id;
 	private String name;
 	
-	@Transient
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	
